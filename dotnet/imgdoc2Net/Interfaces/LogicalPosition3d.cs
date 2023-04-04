361ad8d0c393e2144b9b832c43d3f1c461db9e6c
ptahmose <jbohl@h-quer.de>
2023-04-05 00:18:19 +0200

    cosmetic

--- dotnet/imgdoc2Net/Interfaces/LogicalPosition3d.cs
+++ dotnet/imgdoc2Net/Interfaces/LogicalPosition3d.cs
@@ -9,24 +9,28 @@ namespace ImgDoc2Net.Interfaces
     /// </summary>
     public struct LogicalPosition3d
     {
-        /// <summary>   Gets or sets the x-position in pixel-coordinate-system of the top-left corner of the brick. </summary>
-        /// <value type="double">   The x-position in pixel-coordinate-system of the top-left corner of the brick. </value>
+        /// <summary>   Gets or sets the x-position in pixel-coordinate-system of the front-top-left corner of the brick. </summary>
+        /// <value type="double">   The x-position in pixel-coordinate-system of the front-top-left corner of the brick. </value>
         public double PositionX { get; set; }
 
-        /// <summary>   Gets or sets the y-position in pixel-coordinate-system of the top-left corner of the brick. </summary>
-        /// <value type="double">   The y-position in pixel-coordinate-system of the top-left corner of the brick. </value>
+        /// <summary>   Gets or sets the y-position in pixel-coordinate-system of the front-top-left corner of the brick. </summary>
+        /// <value type="double">   The y-position in pixel-coordinate-system of the front-top-left corner of the brick. </value>
         public double PositionY { get; set; }
 
+        /// <summary>   Gets or sets the z-position in pixel-coordinate-system of the front-top-left corner of the brick. </summary>
+        /// <value type="double">   The z-position in pixel-coordinate-system of the front-top-left corner of the brick. </value>
         public double PositionZ { get; set; }
 
-        /// <summary>   Gets or sets the width in pixel-coordinate-system of the top-left corner of the tile. </summary>
-        /// <value type="double">   The width in pixel-coordinate-system of the top-left corner of the tile. </value>
+        /// <summary>   Gets or sets the width in pixel-coordinate-system of the brick. </summary>
+        /// <value type="double">   The width in pixel-coordinate-system of the brick. </value>
         public double Width { get; set; }
 
-        /// <summary>   Gets or sets the height in pixel-coordinate-system of the top-left corner of the tile. </summary>
-        /// <value type="double">   The height in pixel-coordinate-system of the top-left corner of the tile. </value>
+        /// <summary>   Gets or sets the height in pixel-coordinate-system of the brick. </summary>
+        /// <value type="double">   The height in pixel-coordinate-system of the brick. </value>
         public double Height { get; set; }
 
+        /// <summary>   Gets or sets the depth in pixel-coordinate-system of the brick. </summary>
+        /// <value type="double">   The depth in pixel-coordinate-system of the brick. </value>
         public double Depth { get; set; }
 
         /// <summary>   Gets or sets the pyramid level of the tile. </summary>
