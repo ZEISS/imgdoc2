2871f224fcd3de87bfa46a25892218d09b87ee9c
ptahmose <jbohl@h-quer.de>
2023-04-07 23:45:07 +0200

    more tests

--- libimgdoc2_tests/docinfo3d_test.cpp
+++ libimgdoc2_tests/docinfo3d_test.cpp
@@ -7,6 +7,7 @@
 #include <array>
 #include <random>
 
+#include "utilities.h"
 #include "../libimgdoc2/inc/imgdoc2.h"
 
 using namespace std;
@@ -162,3 +163,139 @@ TEST(DocInfo3d, GetMBoundingBoxForXyzForRandomDocumentAndCheckResult)
     ASSERT_EQ(interval_y, interval_y_partial);
     ASSERT_EQ(interval_z, interval_z_partial);
 }
+
+struct VariousNumberOfBricksFixture : public testing::TestWithParam<int> {};
+
+TEST_P(VariousNumberOfBricksFixture, GetTotalTileCountForSimpleDocumentAndCheckResult)
+{
+    // arrange
+    const int num_tiles = GetParam();
+    const auto create_options = ClassFactory::CreateCreateOptionsUp();
+    create_options->SetDocumentType(DocumentType::kImage3d);
+    create_options->SetFilename(":memory:");
+    create_options->AddDimension('l');
+    create_options->SetUseSpatialIndex(false);
+    create_options->SetCreateBlobTable(false);
+    const auto doc = ClassFactory::CreateNew(create_options.get());
+    const auto writer = doc->GetWriter3d();
+
+    // we just add the specified number of tiles to the document (at layer 0)
+    for (int l = 0; l < num_tiles; ++l)
+    {
+        LogicalPositionInfo3D position_info_3d;
+        BrickBaseInfo brick_base_info;
+        TileCoordinate tc({ { 'l', l } });
+        position_info_3d.posX = 0;
+        position_info_3d.posY = 0;
+        position_info_3d.posZ = 0;
+        position_info_3d.width = 10;
+        position_info_3d.height = 10;
+        position_info_3d.depth = 12;
+        position_info_3d.pyrLvl = 0;
+        brick_base_info.pixelWidth = 10;
+        brick_base_info.pixelHeight = 10;
+        brick_base_info.pixelDepth = 10;
+        brick_base_info.pixelType = 0;
+        writer->AddBrick(&tc, &position_info_3d, &brick_base_info, DataTypes::ZERO, TileDataStorageType::Invalid, nullptr);
+    }
+
+    const auto reader = doc->GetReader3d();
+
+    // act
+    const auto number_of_tiles = reader->GetTotalTileCount();
+
+    // assert
+    EXPECT_EQ(number_of_tiles, num_tiles);
+}
+
+TEST_P(VariousNumberOfBricksFixture, GetTilePerLayerCountForSimpleDocumentAndCheckResult)
+{
+    // arrange
+    const int num_tiles = GetParam();
+    const auto create_options = ClassFactory::CreateCreateOptionsUp();
+    create_options->SetDocumentType(DocumentType::kImage3d);
+    create_options->SetFilename(":memory:");
+    create_options->AddDimension('l');
+    create_options->SetUseSpatialIndex(false);
+    create_options->SetCreateBlobTable(false);
+    const auto doc = ClassFactory::CreateNew(create_options.get());
+    const auto writer = doc->GetWriter3d();
+
+    // we just add the specified number of tiles to the document on layer 0
+    for (int l = 0; l < num_tiles; ++l)
+    {
+        LogicalPositionInfo3D position_info;
+        BrickBaseInfo tileInfo;
+        TileCoordinate tc({ { 'l', l } });
+        position_info.posX = 0;
+        position_info.posY = 0;
+        position_info.posZ = 0;
+        position_info.width = 10;
+        position_info.height = 10;
+        position_info.depth = 10;
+        position_info.pyrLvl = 0;
+        tileInfo.pixelWidth = 10;
+        tileInfo.pixelHeight = 10;
+        tileInfo.pixelDepth = 10;
+        tileInfo.pixelType = 0;
+        writer->AddBrick(&tc, &position_info, &tileInfo, DataTypes::ZERO, TileDataStorageType::Invalid, nullptr);
+    }
+
+    // ...and, half of them on layer 1
+    for (int l = 0; l < num_tiles / 2; ++l)
+    {
+        LogicalPositionInfo3D position_info;
+        BrickBaseInfo tileInfo;
+        TileCoordinate tc({ { 'l', l } });
+        position_info.posX = 0;
+        position_info.posY = 0;
+        position_info.posZ = 0;
+        position_info.width = 10;
+        position_info.height = 10;
+        position_info.depth = 10;
+        position_info.pyrLvl = 1;
+        tileInfo.pixelWidth = 10;
+        tileInfo.pixelHeight = 10;
+        tileInfo.pixelDepth = 10;
+        tileInfo.pixelType = 0;
+        writer->AddBrick(&tc, &position_info, &tileInfo, DataTypes::ZERO, TileDataStorageType::Invalid, nullptr);
+    }
+
+    // ...and, a quarter of them on layer 2
+    for (int l = 0; l < num_tiles / 4; ++l)
+    {
+        LogicalPositionInfo3D position_info;
+        BrickBaseInfo tileInfo;
+        TileCoordinate tc({ { 'l', l } });
+        position_info.posX = 0;
+        position_info.posY = 0;
+        position_info.posZ = 0;
+        position_info.width = 10;
+        position_info.height = 10;
+        position_info.depth = 10;
+        position_info.pyrLvl = 2;
+        tileInfo.pixelWidth = 10;
+        tileInfo.pixelHeight = 10;
+        tileInfo.pixelDepth = 10;
+        tileInfo.pixelType = 0;
+        writer->AddBrick(&tc, &position_info, &tileInfo, DataTypes::ZERO, TileDataStorageType::Invalid, nullptr);
+    }
+
+    const auto reader = doc->GetReader3d();
+
+    // act
+    auto number_of_tiles = reader->GetTotalTileCount();
+    auto tile_count_per_layer = reader->GetTileCountPerLayer();
+
+    // assert
+    EXPECT_EQ(number_of_tiles, num_tiles + num_tiles / 2 + num_tiles / 4);
+
+    // we expect the number of tiles per layer to be correct
+    map<int, std::uint64_t> expected_tile_count_per_layer = { { 0, num_tiles }, { 1, num_tiles / 2 }, { 2, num_tiles / 4 } };
+    ASSERT_TRUE(mapsEqual(tile_count_per_layer, expected_tile_count_per_layer));
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    DocInfo3d,
+    VariousNumberOfBricksFixture,
+    testing::Values(5, 13, 27));
